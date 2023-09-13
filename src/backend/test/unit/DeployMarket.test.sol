bb01608670cb0b249bd167b00a07279a1eec503c
Daniel-Rammirez <rammirez.daniel@gmail.com>
2023-09-13 22:01:58 +1000

    finish tests of makeItem function

--- src/backend/test/unit/DeployMarket.test.sol
+++ src/backend/test/unit/DeployMarket.test.sol
@@ -8,12 +8,24 @@ import {NFT} from "../../src/NFT.sol";
 import {Marketplace} from "../../src/Marketplace.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-contract DeployMoodNftTest is Test {
+contract DeployNFTMarketTest is Test {
     DeployNFTMarket public deployer;
     uint256 constant FEE_PERCENT = 1;
     uint256 constant FIRST_NFT_COUNT = 1;
+    uint256 constant BALANCE_OF_FIRST_TRANSFER = 1;
     NFT nft;
     Marketplace marketplace;
+
+    event Offered(uint256 itemId, address indexed nft, uint256 tokenId, uint256 price, address indexed seller);
+
+    // struct Item {
+    //     uint256 itemId;
+    //     IERC721 nft;
+    //     uint256 tokenId;
+    //     uint256 price;
+    //     address payable seller;
+    //     bool sold;
+    // }
 
     address USER = makeAddr("user");
     string public constant URI =
@@ -43,25 +55,63 @@ contract DeployMoodNftTest is Test {
         assert(FEE_PERCENT == marketplace.feePercent());
     }
 
-    function testGetingTheTotalPrice() public {
-        uint256 initial_price = 10 ether;
-        uint256 expected_total_price = initial_price * (100 + FEE_PERCENT) / 100;
-        uint256 token_id = 1;
+    uint256 initial_price = 10 ether;
+    uint256 expected_total_price = initial_price * (100 + FEE_PERCENT) / 100;
+    uint256 token_id;
 
+    modifier userMintAndMakeItem() {
         vm.startPrank(USER);
         nft.mint(URI);
+        token_id = nft.tokenCount();
         nft.setApprovalForAll(address(marketplace), true);
         marketplace.makeItem(IERC721(address(nft)), token_id, initial_price);
         vm.stopPrank();
+        _;
+    }
+
+    function testGetingTheTotalPrice() public userMintAndMakeItem {
         assert(marketplace.getTotalPrice(token_id) == expected_total_price);
     }
 
-    function testMakeItem() public {
+    function testMakeItem() public userMintAndMakeItem {
         // check:
         // itemCount
+        assert(token_id == FIRST_NFT_COUNT);
         // transferFrom
-        // mappgin itemCount
-        // event
-        // rever if require fail
+        assert(nft.balanceOf(address(marketplace)) == BALANCE_OF_FIRST_TRANSFER);
+        assert(nft.ownerOf(token_id) == address(marketplace));
+        // mappgin items[itemCount]
+        uint256 itemCount = marketplace.itemCount();
+        (uint256 _itemCount, IERC721 _nft, uint256 _tokenId, uint256 _price, address seller, bool sold) =
+            marketplace.items(itemCount);
+        assert(_itemCount == itemCount);
+        assert(_nft == nft);
+        assert(_tokenId == token_id);
+        assert(_price == initial_price);
+        assert(seller == address(USER));
+        assert(sold == false);
+    }
+
+    function testEventMakeItem() public {
+        vm.startPrank(USER);
+        nft.mint(URI);
+        token_id = nft.tokenCount();
+        uint256 itemCount = marketplace.itemCount();
+        nft.setApprovalForAll(address(marketplace), true);
+        vm.expectEmit(true, true, false, false);
+        emit Offered(itemCount, address(nft), token_id, initial_price, address(USER));
+        marketplace.makeItem(IERC721(address(nft)), token_id, initial_price);
+        vm.stopPrank();
+    }
+
+    function testRevertPriceNotEnough() public {
+        uint256 PRIZE_ZERO = 0;
+        vm.startPrank(USER);
+        nft.mint(URI);
+        token_id = nft.tokenCount();
+        nft.setApprovalForAll(address(marketplace), true);
+        vm.expectRevert();
+        marketplace.makeItem(IERC721(address(nft)), token_id, PRIZE_ZERO);
+        vm.stopPrank();
     }
 }
